19824ea63954bc86eba59132d7bdce24345913e0
Sabine.Eitel <Seit201@aol.com>
2022-11-09 12:00:46 +0100

    AdditionZahlen

--- challenge_01.js
+++ challenge_01.js
@@ -9,8 +9,34 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
-let ZahlA = parseInt(prompt("erste Zahl:"));
-let ZahlB = parseInt(prompt("zweite Zahl:"));
-let summe = ZahlA + ZahlB
+// let ZahlA = parseInt(prompt("erste Zahl:"));
+// let ZahlB = parseInt(prompt("zweite Zahl:"));
+// let summe = ZahlA + ZahlB
 
+// console.log("Die Summe der Zahlen ist: " + summe);
+
+
+/************ Variante 1 *************/
+
+// Deklaration
+let zifferA, zifferB
+let ZahlA, ZahlB
+let summe
+
+ziffer1 = prompt("Zahl 1:");
+ziffer2 = prompt("Zahl 2:");
+console.log(typeof ziffer1);
+console.log(typeof ziffer2);
+
+// Eingabe - Wertzuweisung :: Ziffern
+ZahlA = parseInt(ziffer1);
+ZahlB = parseInt(ziffer2);
+console.log(typeof ZahlA);
+console.log(typeof ZahlB);
+
+
+// Berechnung :: Summe / Addition
+summe = ZahlA + ZahlB;
+
+// Ausgabe
 console.log("Die Summe der Zahlen ist: " + summe);
